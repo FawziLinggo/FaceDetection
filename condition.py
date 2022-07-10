2d3067e8d07042a41326d7d3410da4b261503cdb
FawziLinggo <fawzilinggo@gmail.com>
2022-07-10 10:59:21 +0700

    Menambahkan while loop

--- condition.py
+++ condition.py
@@ -34,42 +34,47 @@ path_save_homomorpic = "images/HasilFiltering/HomomorphicFilter-"
 path_save_face_detection = "images/HasilFiltering/FaceDetection-"
 
 """ Untuk Path Foto """
-path=""
+path =""
 
 """ Path Detector """
 detector = dlib.get_frontal_face_detector()
 
 def run_program(path):
-    print("Jenis-Jenis Foto \n"
-          "1. jarak 1 Meter \n"
-          "2. jarak 60 Meter \n"
-          "3. jarak 100 Meter \n"
-          "4. jarak 150 Meter \n"
-          "Pilih sesuai angka (Harus Angka)"
-          )
-    Hello = int(input("Masukkan Angka : "))
-    if (Hello == 1):
-        subject = "1-meter.jpg"
-        path += meter_1
-        logging.info("Menjalankan Program dengan jarak 1 meter")
-        homomorphic(path,subject,Hello)
-    elif (Hello == 2):
-        subject = "60-meter.jpg"
-        path += meter_60
-        logging.info("Menjalankan Program dengan jarak 60 meter")
-        homomorphic(path, subject, Hello)
-    elif (Hello == 3):
-        subject = "100-meter.jpg"
-        path += meter_100
-        logging.info("Menjalankan Program dengan jarak 100 meter")
-        homomorphic(path,subject,Hello)
-    elif (Hello == 4):
-        subject = "150-meter.jpg"
-        path += meter_150
-        logging.info("Menjalankan Program dengan jarak 150 meter")
-        homomorphic(path,subject,Hello)
-    else:
-        print(" ERROR : must number :)")
+    while(True):
+        print("\n ++++++ Jenis-Jenis Foto ++++++ \n"
+              "1. jarak 1 Meter \n"
+              "2. jarak 60 Meter \n"
+              "3. jarak 100 Meter \n"
+              "4. jarak 150 Meter \n"
+              "5. Exit Program \n"
+              " ++++++ Pilih sesuai angka (Harus Angka) ++++++"
+              )
+        Hello = int(input("Masukkan Angka : "))
+        if (Hello == 1):
+            subject = "1-meter.jpg"
+            path += meter_1
+            logging.info("Menjalankan Program dengan jarak 1 meter")
+            homomorphic(path, subject, Hello)
+        elif (Hello == 2):
+            subject = "60-meter.jpg"
+            path += meter_60
+            logging.info("Menjalankan Program dengan jarak 60 meter")
+            homomorphic(path, subject, Hello)
+        elif (Hello == 3):
+            subject = "100-meter.jpg"
+            path += meter_100
+            logging.info("Menjalankan Program dengan jarak 100 meter")
+            homomorphic(path, subject, Hello)
+        elif (Hello == 4):
+            subject = "150-meter.jpg"
+            path += meter_150
+            logging.info("Menjalankan Program dengan jarak 150 meter")
+            homomorphic(path, subject, Hello)
+        elif (Hello == 5):
+            break
+        else:
+            print(" ERROR : must number :)")
+
 
 def homomorphic(path, subject,Hello, path_save_homomorpic=path_save_homomorpic):
     try:
@@ -106,8 +111,9 @@ def homomorphic(path, subject,Hello, path_save_homomorpic=path_save_homomorpic):
             # cv2.namedWindow('Gambar Filtering', cv2.WINDOW_KEEPRATIO)
             # cv2.imshow("Gambar Filtering", img_filtered)
             # cv2.waitKey(0)
-
+            logging.info("Memanggil fungsi Face Detector")
             face_detector(path_save_homomorpic,subject)
+
 
     except:
         logging.error("Error saat melakukan Filtering : %s", OSError)
@@ -125,19 +131,16 @@ def face_detector(path_save_homomorpic,subject, path_save_face_detection=path_sa
         """Perintah ini untuk menampilkan Bounding Box"""
         kotak = cv2.rectangle(crop, (x1, y1), (x2, y2), (0, 255, 255), 4)
         kotak = kotak[y1:y2, x1:x2]
+        logging.info("Sukses Membuat bounding box")
 
-        # Menampilkan Kotak
-        # cv2.namedWindow('Hanya Kotak', cv2.WINDOW_KEEPRATIO)
-        # cv2.imshow("Hanya Kotak", kotak)
         path_save_face_detection += subject
         cv2.imwrite(path_save_face_detection,kotak)
+        logging.info("Menyimpan hasil bounding box")
         morfologi(path_save_face_detection)
 
 def morfologi(path_save_face_detection):
     img = cv2.imread(path_save_face_detection, 0)
-
     binr = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
     kernel = np.ones((5, 5), np.uint8)
 
     # invert the image
@@ -145,50 +148,55 @@ def morfologi(path_save_face_detection):
 
     # erode the image
     erosion = cv2.erode(invert, kernel, iterations=1)
+    logging.info("Berhasil melakukan Morfologi erosi")
 
     plt.figure(figsize=(8, 7))
     plt.subplot(2, 2, 1)
     plt.imshow(erosion, cmap='gray')
     plt.title('erosion')
     plt.axis('off')
+    logging.info("Menampilkan Morfologi erosi")
 
     # define the kernel
     kernel = np.ones((3, 3), np.uint8)
-
-    # invert the image
     invert = cv2.bitwise_not(binr)
-
-    # dilate the image
     dilation = cv2.dilate(invert, kernel, iterations=1)
+    logging.info("Berhasil melakukan Morfologi dilasi")
 
     # print the output
     plt.subplot(2, 2, 2)
     plt.imshow(dilation, cmap='gray')
     plt.title('dilation')
     plt.axis('off')
+    logging.info("Menampilkan Morfologi dilasi")
+
 
     # define the kernel
     kernel = np.ones((3, 3), np.uint8)
-
-    # opening the image
     opening = cv2.morphologyEx(binr, cv2.MORPH_OPEN,
                                kernel, iterations=1)
+    logging.info("Berhasil melakukan Morfologi opening")
+
     # print the output
     plt.subplot(2, 2, 3)
     plt.imshow(opening, cmap='gray')
     plt.title('opening')
     plt.axis('off')
+    logging.info("Menampilkan Morfologi opening")
+
 
     kernel = np.ones((3, 3), np.uint8)
 
     # opening the image
     closing = cv2.morphologyEx(binr, cv2.MORPH_CLOSE, kernel, iterations=1)
+    logging.info("Berhasil melakukan Morfologi closing")
+
 
     # print the output
     plt.subplot(2, 2, 4)
     plt.imshow(closing, cmap='gray')
     plt.title('closing')
     plt.axis('off')
+    logging.info("Menampilkan Morfologi opening")
     plt.show()
 
-run_program(path)
