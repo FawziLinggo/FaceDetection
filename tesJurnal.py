c1343576ecfaea6dbd7f354567a70d3ab3732d08
FawziLinggo <fawzilinggo@gmail.com>
2022-08-05 19:32:31 +0700

    Bismillah selesai

--- tesJurnal.py
+++ tesJurnal.py
@@ -1,11 +1,6 @@
 import cv2
-import dlib
 import numpy as np
-import os,random
-# lendmark_path = "model/shape_predictor_68_face_landmarks.dat"
-#
-# detector = dlib.get_frontal_face_detector()
-# predictor =dlib.shape_predictor(lendmark_path)
+
 
 img2 = cv2.imread("images/HasilFiltering/FaceDetection-1-meter.jpg")
 
@@ -13,11 +8,12 @@ img2 = cv2.imread("images/HasilFiltering/FaceDetection-1-meter.jpg")
 imgGray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 binr = cv2.threshold(imgGray2, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
-invert=cv2.bitwise_not(binr)
+
+# Kernel Dilasi
 kernel = np.ones((25, 25), np.uint16)
 dilation = cv2.dilate(binr, kernel, iterations=7)
 
-
+# Kernel Closing
 kernel1 = np.ones((25, 25), np.uint16)
 closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel1, iterations=10)
 
